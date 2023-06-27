9b0d3fa4ff60d6b1be8acb5551b0e0e09ad9d8e1
Alysson Gabriel <115495030+AlyssonSmartCell@users.noreply.github.com>
2023-06-26 22:45:51 -0300

    finalizado slide curso mobile

--- JS.js
+++ JS.js
@@ -17,3 +17,62 @@ function proximaImg(){
     }
     document.getElementById('radio'+cont).checked = true;
 }; //Função conplementar da Arrowfunction
+
+
+
+
+
+
+
+// ===============================================Mobile================================================
+
+const control = document.querySelectorAll('.control');
+
+let currentItem = 0;
+
+const items = document.querySelectorAll('.item ');
+
+const maxItems = items.length;
+
+
+control.forEach(control =>{
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrow-left')
+        console.log('control clicked', isLeft, currentItem)
+
+        if(isLeft){
+            currentItem -= 1;
+        }else{
+            currentItem += 1;
+        }
+
+        if(currentItem >= maxItems){
+            currentItem = 0;
+        }
+
+        if(currentItem < 0){
+            currentItem = maxItems -1;
+        }
+        
+        items.forEach(item => item.classList.remove('current-item'));
+
+        items[currentItem].scrollIntoView({
+            behavior:"smooth",
+            inline:"center"
+
+            
+        });
+        items[currentItem].classList.add('current-item');
+
+
+        });
+            
+
+
+
+});
+
+
+
+
+
